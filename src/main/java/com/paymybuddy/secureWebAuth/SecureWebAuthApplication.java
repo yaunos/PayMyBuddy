5634b95c7c5f881c75994927291df2f047089e3b
DevPC <a.roche.c@gmail.com>
2023-04-21 14:09:14 +0200

    Web dependencies and datasource configurations initial commit

---
+++ src/main/java/com/paymybuddy/secureWebAuth/SecureWebAuthApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.secureWebAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWebAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/secureWebAuth/SecureWebAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.secureWebAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWebAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
